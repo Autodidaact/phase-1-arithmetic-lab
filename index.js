7ee323235493df18595bab5cc64b74897f228c0b
Ibrahim Abdulai Yakubu <ibrahimay295@gmail.com>
2022-06-07 15:40:40 +0000

    Modified index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+// Write your code 
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10);
+
+
+let num3 = 19;
+let num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(1, 3, 10, 5, 20);
+
